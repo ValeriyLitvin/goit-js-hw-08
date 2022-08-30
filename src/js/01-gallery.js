7e59baeb0b81b38b826a28465dc5e9c1930619cc
ValeriyLitvin <valerkosssss@gmail.com>
2022-08-30 20:43:42 +0300

    first is ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryDiv = document.querySelector('.gallery');
+const createImaneFn = createImageCard({ galleryItems });
+
+galleryDiv.insertAdjacentHTML('beforeend', createImaneFn);
+
+galleryDiv.addEventListener('click', callOriginalPicture);
+
+function createImageCard({ galleryItems }) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+             </a>`;
+    })
+    .join('');
+}
+function callOriginalPicture(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  let lightbox = new SimpleLightbox('.gallery a', {
+    navText: ['👈', '👉'],
+    captionsData: 'alt',
+    showCounter: false,
+    captionDelay: 250,
+  });
+}
