ddda1b2f81d6632c977dc84b93036bfc4b471d10
ValeriyLitvin <valerkosssss@gmail.com>
2022-08-31 21:44:33 +0300

    end 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+  console.log(currentTime);
+};
+
+const timePlayerNow = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(timePlayerNow);
+
+player.on('timeupdate', throttle(onPlay, 1000));
